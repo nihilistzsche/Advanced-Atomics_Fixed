806e6cb4161f5356bb228616ec77ca705c355403
Nihilistzsche <nihilistzsche@gmail.com>
2022-06-12 22:38:00 -0700

    Update to 1.1 and update to the mod structure I use for development.

---
+++ prototypes/plutonium-armor.lua
@@ -0,0 +1,69 @@
+data:extend(
+    {
+  --- Plutonium Armor
+  {
+    type = "armor",
+    name = "plutonium-armor",
+    icon = "__Advanced-Atomics_Fixed__/graphics/icons/plutonium-armor.png",
+    icon_size = 32,
+    flags = {},
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 30,
+        percent = 60
+      },
+      {
+        type = "acid",
+        decrease = 30,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 80,
+        percent = 60
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 100
+      }
+    },
+    durability = 40000,
+    subgroup = "armor",
+    order = "g[plutonium-armor]",
+    stack_size = 1,
+    equipment_grid = "largest-equipment-grid",
+    inventory_size_bonus = 60
+  },
+
+  {
+    type = "recipe",
+    name = "plutonium-armor",
+    enabled = false,
+    energy_required = 120,
+    ingredients =
+    {
+      {"uranium-armor", 1},
+      {"fusion-reactor-equipment", 2},
+      {"processing-unit", 300},
+      {"low-density-structure", 200},
+      {"plutonium-239", 42}
+    },
+    result = "plutonium-armor",
+    requester_paste_multiplier = 1
+  }
+
+})
+
+for _, animation in ipairs(data.raw["character"]["character"]["animations"]) do
+  if animation.armors then
+    for _, armor in ipairs(animation.armors) do
+      if armor == "power-armor-mk2" then
+        animation.armors[#animation.armors + 1] = "plutonium-armor"
+        break
+      end
+    end
+  end
+end
---
+++ prototypes/plutonium.lua
@@ -0,0 +1,138 @@
+data:extend(
+{
+    {
+    type = "item",
+    name = "plutonium-239",
+    icon = "__Advanced-Atomics_Fixed__/graphics/icons/plutonium-239.png",
+    icon_size = 32,
+    flags = {},
+    subgroup = "intermediate-product",
+    order = "g[uranium-238]z",
+    stack_size = 50
+    },
+
+    {
+    type = "recipe",
+    name = "plutonium-nucleosynthesis",
+    energy_required = 42,
+    enabled = false,
+    category = "centrifuging",
+    ingredients = {{"uranium-235", 42}, {"uranium-238", 5}},
+    icon = "__Advanced-Atomics_Fixed__/graphics/icons/plutonium-processing.png",
+    icon_size = 32,
+    subgroup = "intermediate-product",
+    order = "r[uranium-processing]z",
+    allow_decomposition = false,
+    main_product = "",
+    results =
+    {
+      {
+        name = "plutonium-239",
+        probability = 0.4,
+        amount = 5
+      },
+      {
+        name = "uranium-238",
+        probability = 0.6,
+        amount = 5
+      },
+      {
+        name = "uranium-235",
+        probability = 0.99,
+        amount = 42
+      }
+    }
+  },
+
+  -- Plutonium fuel
+  {
+    type = "item",
+    name = "plutonium-fuel",
+    icon = "__Advanced-Atomics_Fixed__/graphics/icons/plutonium-fuel.png",
+    icon_size = 32,
+    flags = {},
+    fuel_category = "chemical",
+    fuel_value = "1.8GJ",
+    fuel_acceleration_multiplier = 3,
+    fuel_top_speed_multiplier = 1.25,
+    subgroup = "intermediate-product",
+    order = "r[plutonium-fuel]",
+    stack_size = 1
+  },
+  {
+    type = "recipe",
+    name = "plutonium-fuel",
+    energy_required = 60,
+    enabled = false,
+    category = "centrifuging",
+    subgroup = "intermediate-product",
+    order = "r[plutonium-fuel]",
+    ingredients = {
+      {"plutonium-239", 2},
+      {"rocket-fuel", 1}
+    },
+    icon = "__Advanced-Atomics_Fixed__/graphics/icons/plutonium-fuel.png",
+    icon_size = 32,
+    results =
+    {
+      {
+        name = "plutonium-fuel",
+        probability = 0.9,
+        amount = 1
+      },
+      {
+        name = "nuclear-fuel",
+        probability = 0.1,
+        amount = 1
+      }
+    }
+  },
+
+--- mox-fuel-cell
+  {
+    type = "item",
+    name = "mox-fuel-cell",
+    icon = "__Advanced-Atomics_Fixed__/graphics/icons/mox-fuel-cell.png",
+    icon_size = 32,
+    flags = {},
+    subgroup = "intermediate-product",
+    order = "r[uranium-processing]-b[mox-fuel-cell]",
+    fuel_category = "nuclear",
+    burnt_result = "used-up-uranium-fuel-cell",
+    fuel_value = "12GJ",
+    stack_size = 50
+  },
+  {
+    type = "recipe",
+    name = "mox-fuel-cell",
+    energy_required = 10,
+    enabled = false,
+    ingredients =
+    {
+      {"steel-plate", 10},
+      {"uranium-235", 1},
+      {"plutonium-239", 1},
+      {"uranium-238", 18}
+    },
+    result = "mox-fuel-cell",
+    result_count = 10
+  },
+
+})
+
+if not AA.ENABLED["SSP4Plutonium"] then
+--- modify nuclear-fuel-reprocessing
+data.raw["recipe"]["nuclear-fuel-reprocessing"].results =
+  {
+    {
+      name = "uranium-238",
+      amount = 3,
+      probability = 1.0,
+    },
+    {
+      name = "plutonium-239",
+      amount = 1,
+      probability = 0.1,
+    }
+  }
+end
